cb9a3e6ead016e9b0c6a4d321e7d7f46617e936a
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-07-21 19:16:14 +0700

    :hammer: build: refactor CI.CD github action #2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: "",
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   typescript: {
     ignoreBuildErrors:
       process.env.NEXT_PUBLIC_TS_IGNORE_BUILD_ERRORS === "true",
@@ -9,11 +10,12 @@ const nextConfig = {
     ignoreDuringBuilds:
       process.env.NEXT_PUBLIC_ESLINT_IGNORE_DURING_BUILDS === "true",
   },
-  output: process.env.NEXT_PUBLIC_OUTPUT || "standalone",
+  output: "export",
+  // output: process.env.NEXT_PUBLIC_OUTPUT || "standalone",
   reactStrictMode: true,
-  images: {
-    disableStaticImages: true,
-  },
+  // images: {
+  //   disableStaticImages: true,
+  // },
 };
 
 export default nextConfig;
